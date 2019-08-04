3492185f88e9ece8a48c303b5f6ae27af67a85fb
Michał Burdzy <mmicalt@gmail.com>
2019-08-04 23:20:50 +0200

    initial commit

---
+++ ankieta/models.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models
+from django.utils.timezone import now
+# Create your models here.
+
+
+class Pytanie(models.Model):
+    text_pytania = models.CharField(max_length=200)
+    data_publikacji = models.DateTimeField(default=now, editable=False)
+
+    def siemanko(self):
+        return 'Siemanko z pytania nr : {}'.format(self.id)
+
+    def __str__(self):
+        return self.text_pytania
+
+
+class Odpowiedz(models.Model):
+    pytanie = models.ForeignKey(Pytanie, on_delete=models.CASCADE)
+    odpowiedz = models.CharField(max_length=200)
+    glosy = models.IntegerField(default=0)
+
+    def __str__(self):
+        return self.odpowiedz
